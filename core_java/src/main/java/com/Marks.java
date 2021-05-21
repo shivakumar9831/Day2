a6df654e8d0fdb4cac12c120bc8aa969851ea4f6
SHIVA <SHIVA@DESKTOP-UCIBHQ6>
2021-05-21 15:39:08 +0530

    my project

---
+++ core_java/src/main/java/com/Marks.java
@@ -0,0 +1,32 @@
+package com;
+
+public class Marks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int marks=60;
+		
+		
+			if(marks>=88 && marks>=65 )
+			{
+				System.out.println("first class");
+			}
+			else if(marks<=66 && marks>=50) {
+				System.out.println("Second class");
+			}
+			else if(marks<=51 && marks>=40) {
+				System.out.println("Third class");
+			}
+			else {
+				System.out.println("fail");
+			}
+			
+		}
+
+	}
+
+
+
+
